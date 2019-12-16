4b49b18c00866b63224aaf05724e4efb6021fbed
jarlehansen <jarle@jarlehansen.net>
2019-12-16 08:36:50 +0100

    updated

---
+++ src/main/java/net/jarlehansen/MyService.java
@@ -0,0 +1,7 @@
+package net.jarlehansen;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
---
+++ src/test/groovy/net/jarlehansen/MyServiceSpec.groovy
@@ -0,0 +1,9 @@
+package net.jarlehansen
+
+import spock.lang.Specification
+
+class MyServiceSpec extends Specification {
+
+    def ""() {
+    }
+}
